0fee0e85c3e01c1ae7ee4cd8df45b575752f907a
Fernando Merlo <fdomerlo@gmail.com>
2026-09-12 11:14:40 -0300

    Update devai.sh

--- scripts/devai.sh
+++ scripts/devai.sh
@@ -13,10 +13,10 @@ else
     echo "==> antigravity CLI is already installed"
 fi
 
-if ! command -v claude &> /dev/null; then
-    curl -fsSL https://claude.ai/install.sh | bash
-else
-    echo "==> claude is already installed"
-fi
+#if ! command -v claude &> /dev/null; then
+#    curl -fsSL https://claude.ai/install.sh | bash
+#else
+#    echo "==> claude is already installed"
+#fi
 
 bash scripts/setup_agy.sh
